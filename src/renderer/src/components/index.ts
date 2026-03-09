cf025d09727176141438231c0026c92ff513a315
dandelion <1993466159@qq.com>
2026-03-09 16:21:50 +0800

    fix(src): 修复windows git大小写不敏感导致文件改名未被提交上去导致ts检查报错

--- src/renderer/src/components/index.ts
+++ src/renderer/src/components/index.ts
@@ -1,2 +1,2 @@
-export { DraftIcon, ExclamationIcon, LightningIcon } from "./icon/Icon";
-export * from "./icon/types";
+export { DraftIcon, ExclamationIcon, LightningIcon } from './icon/Icon'
+export * from './icon/types'
